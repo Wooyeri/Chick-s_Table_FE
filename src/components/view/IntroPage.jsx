00f5996a56fc923bea52dc0db2ebc99587e662a3
mjkwahk <mjkwahk@gmail.com>
2024-09-25 18:30:09 +0900

    Intro Page (채팅 이미지 X)

--- src/components/view/IntroPage.jsx
+++ src/components/view/IntroPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './IntroPage.css';
 
 const Intro = () => {
-    return <h1>Welcome to the Intro Page</h1>;
+    return (
+        <div className='text-container'>
+            <div className='main-title'>
+                당신의 <span className='bold'>식탁</span>
+                <span className='line-break'></span>
+                <span className='bold'>건강</span>하게 지켜보세요!
+            </div>
+            <div className='sub-title'>
+                우리 <span className='bold-green'>병아리식탁</span>은 사용자와의 AI 상담을 통해
+                <span className='line-break'></span>
+                사용자 맞춤 식단 레시피를 제공합니다.
+            </div>
+        </div>
+    );
 };
 
 export default Intro;
