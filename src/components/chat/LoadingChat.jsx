74e682156d108a678316ea4ae46a028cabb8d55b
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-09-29 15:09:12 +0900

    Add LoadingChat
    * Animation not included

--- src/components/chat/LoadingChat.jsx
+++ src/components/chat/LoadingChat.jsx
@@ -1,1 +1,22 @@
-export default function LoadingChat(){}
+export default function LoadingChat(){
+    const containerStyles = {
+        display: "flex",
+        width: "100%",
+        paddingBottom: "1.5rem",
+        justifyContent: "left"
+    }
+    const bubbleStyle = {
+        width: 'fit-content',
+        padding: '2rem',
+        background: '#6AACA5',
+        border: '1px solid rgba(112, 124, 151, 0.25)',
+        borderRadius: '16px 16px 16px 0px',
+    }
+    return(
+        <div style={containerStyles}>
+            <div style={bubbleStyle}>
+                불러오는 중..
+            </div>
+        </div>
+    )
+}
