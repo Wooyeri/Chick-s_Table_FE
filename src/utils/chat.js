9fdc6743e2f5218ff31e508a1f7e5c7ad52022e1
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-09-30 01:33:12 +0900

    Use better model

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY)
-const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
 export var chat;
 
 export const startChat = (history) => {
