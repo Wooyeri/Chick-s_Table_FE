515448844f6e32bd4b7ba75af0bc57467578d183
mjkwahk <mjkwahk@gmail.com>
2024-09-30 14:42:51 +0900

    Dropdown Menu Edit

--- src/components/layout/HeaderComponent.jsx
+++ src/components/layout/HeaderComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'; // useState 추가
 import './HeaderComponent.css';
 import ChickLoge from '../../assets/chick_text.png';
 import SearchIcon from '../../assets/search-icon.png';
@@ -6,8 +6,22 @@ import ChatbotIcon from '../../assets/chatbot-icon.png';
 import Dropdown from '../../assets/dropdown-icon.png';
 import { Link } from "react-router-dom";
 
-
 const HeaderComponent = () => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false); // 드롭다운 상태 추가
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible); // 드롭다운 토글 함수
+  };
+
+  const renderDropdownMenu = () => {
+    return (
+      <ul className="dropdown-menu">
+        <li className="dropdown-list"><Link to="/login">로그인</Link></li>
+        <li className="dropdown-list"><Link to="/join">회원가입</Link></li>
+      </ul>
+    );
+  };
+
   return (
     <header className="top-bar">
       <div className="logo">
@@ -18,22 +32,28 @@ const HeaderComponent = () => {
       <nav className="header-bar">
         <ul className="header-list">
           <Link to="/recipesearch">
-            <li className="header-item"><img src={SearchIcon} alt="Search Icon" className="search-icon" />레시피 검색</li>
+            <li className="header-item">
+              <img src={SearchIcon} alt="Search Icon" className="search-icon" />레시피 검색
+            </li>
           </Link>
           <div className="vertical-bar"></div>
-          {/* <Link to="/chatbot"> */}
-            <li className="header-item"><img src={ChatbotIcon} alt="Chatbot Icon" className="chatbot-icon" />나만의 영양사</li>          
-          {/* </Link> */}
+          <li className="header-item">
+            <img src={ChatbotIcon} alt="Chatbot Icon" className="chatbot-icon" />나만의 영양사
+          </li>
           <div className="vertical-bar"></div>
-          <li className="dropdown-container">
+          <li className="dropdown-container" onClick={toggleDropdown}>
             <img src={Dropdown} alt="Dropdown" className="dropdown-icon" />
-            {/* <div className={`dropdown-menu ${isDropdownVisible ? "show" : ""}`}>{renderDropdownMenu()}</div> */}
           </li>
         </ul>
       </nav>
+      {/* 드롭다운 메뉴를 헤더 아래에 위치시킴 */}
+      {isDropdownVisible && (
+        <div className="dropdown-menu-container">
+          {renderDropdownMenu()}
+        </div>
+      )}
     </header>
   );
 };
 
 export default HeaderComponent;
-
