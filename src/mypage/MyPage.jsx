a9de38cc6835464c466c6f63e8ce87f0af83099e
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-10-01 16:46:36 +0900

    Change html, body, root styles

--- src/mypage/MyPage.jsx
+++ src/mypage/MyPage.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios'; // axios로 API 호출
-import { useNavigate } from 'react-router-dom'; 
-import './MyPage.css'; 
+import { useNavigate } from 'react-router-dom';
+import './MyPage.css';
 import pencilIcon from '../assets/pencil-icon.png';
 import listIcon from '../assets/list.png';
 
 const MyPage = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [userData, setUserData] = useState({
     id: '',
     email: '',
@@ -27,57 +27,59 @@ const MyPage = () => {
           'Authorization': `Bearer ${token}` // JWT 토큰을 헤더에 포함
         }
       })
-      .then(response => {
-        const user = response.data;
-        // 사용자 데이터 설정
-        setUserData({
-          id: user.id,
-          email: user.email,
-          nickname: user.nickname,
-          image: user.profilePath || 'https://via.placeholder.com/120', // profilePath로 설정
-          diseases: user.diseases || [] // diseases 배열 처리
+        .then(response => {
+          const user = response.data;
+          // 사용자 데이터 설정
+          setUserData({
+            id: user.id,
+            email: user.email,
+            nickname: user.nickname,
+            image: user.profilePath || 'https://via.placeholder.com/120', // profilePath로 설정
+            diseases: user.diseases || [] // diseases 배열 처리
+          });
+        })
+        .catch(error => {
+          console.error('사용자 정보를 가져오는 데 실패했습니다.', error);
         });
-      })
-      .catch(error => {
-        console.error('사용자 정보를 가져오는 데 실패했습니다.', error);
-      });
     } else {
       console.error('유효한 userId 또는 토큰이 없습니다.');
     }
   }, []);
 
   return (
-    <div className="mypage-container">
-      <h2>마이페이지</h2>
-      <div className="profile-section">
-        <img 
-          src={userData.image}  
-          alt="프로필" 
-          className="profile-pic"
-        />
-        <div className="profile-info">
-          <p><strong>Id :</strong> {userData.id}</p>
-          <p><strong>Email :</strong> {userData.email}</p>
-          <p><strong>Nickname :</strong> {userData.nickname}</p>
-          <p>
-            <strong style={{ display: 'inline-block', verticalAlign: 'top' }}>Diseases :</strong>
-            <span className="profile-disease-list">
-              {userData.diseases.map((d, index) => (
-                <span key={index} className="profile-disease-item">
-                  {d.name} {d.contents && `(${d.contents})`}
-                </span>
-              ))}
-            </span>
-          </p>
+    <div className="mypage">
+      <div className='mypage-container'>
+        <h2>마이페이지</h2>
+        <div className="profile-section">
+          <img
+            src={userData.image}
+            alt="프로필"
+            className="profile-pic"
+          />
+          <div className="profile-info">
+            <p><strong>Id :</strong> {userData.id}</p>
+            <p><strong>Email :</strong> {userData.email}</p>
+            <p><strong>Nickname :</strong> {userData.nickname}</p>
+            <p>
+              <strong style={{ display: 'inline-block', verticalAlign: 'top' }}>Diseases :</strong>
+              <span className="profile-disease-list">
+                {userData.diseases.map((d, index) => (
+                  <span key={index} className="profile-disease-item">
+                    {d.name} {d.contents && `(${d.contents})`}
+                  </span>
+                ))}
+              </span>
+            </p>
+          </div>
+          <button className="edit-button" onClick={() => navigate('/mypage/edit')}>
+            <img src={pencilIcon} alt="Edit" style={{ width: '20px', height: '20px', marginRight: '10px' }} />
+          </button>
         </div>
-        <button className="edit-button" onClick={() => navigate('/mypage/edit')}>
-          <img src={pencilIcon} alt="Edit" style={{ width: '20px', height: '20px', marginRight: '10px' }} />
+
+        <button className="saved-chat-button" onClick={() => { navigate('/saved-recipes') }}>
+          저장한 레시피 목록 보러 가기 <img src={listIcon} alt="List Icon" style={{ width: '12px', height: '15px', verticalAlign: '-2px', marginLeft: '5px' }} />
         </button>
       </div>
-
-      <button className="saved-chat-button" onClick={() => {navigate('/saved-recipes')}}>
-        저장한 레시피 목록 보러 가기 <img src={listIcon} alt="List Icon" style={{ width: '12px', height: '15px', verticalAlign: '-2px', marginLeft: '5px'}} />
-      </button>
     </div>
   );
 };
